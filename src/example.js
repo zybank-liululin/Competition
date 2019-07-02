8756363ec706ab1f315ad449edde7eb90e136285
zybank-liululin <liululin@zybank.com.cn>
2019-07-02 11:40:18 +0800

    项目初始化代码

---
+++ src/example.js
@@ -0,0 +1,20 @@
+// export const add = (a, b) => a + b;
+const car  = class {
+    constructor (color) {
+        this.color = color
+    }
+    getColor () {
+        return this.color
+    }
+}
+const car1  = class {
+    constructor (color) {
+        this.color = color
+    }
+    getColor () {
+        return this.color
+    }
+}
+export {car, car1}
+
+
